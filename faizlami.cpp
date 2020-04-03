f731bf5c51193130122be0ea9ea6b710e299d192
ar7L <61224887+ar7L@users.noreply.github.com>
2020-04-03 23:36:23 +0600

    Add files via upload

---
+++ faizlami.cpp
@@ -0,0 +1,106 @@
+/*********************************************************************************************************
+ 
+                                                ___________________         _____________________________
+                /***************\               |                   *       /                             /
+               /                 \              |                     *    /                             /
+              /      ______       \             |                       * /__________________           /
+             /      /      \       \            |      ______________    *                   /         /
+            /      /        \       \           |     |              |                      /         /
+           /      /          \       \          |     |______________|   *                 /         /
+          /      /____________\       \         |      _____            *                 /         /
+         /      _______________\       \        |     |     \         *                  /         /
+        /      /                \       \       |     |      \     *                    /         /
+       /      /                  \       \      |     |       \    \                   /         /
+      /      /                    \       \     |     |        \    \                 /         /
+     /      /                      \       \    |     |         \    \               /         /
+    /      /                        \       \   |     |          \    \             /         /
+   /______/                          \_______\  |_____|           \____\           /_________/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+**************************************************************************************************************************/
+ 
+#include<bits/stdc++.h>
+typedef unsigned long long ll;
+using namespace std;
+ 
+bool isPerfectSquare(int x)
+{
+        int s = sqrt(x);
+        return (s*s == x);
+}
+ 
+bool isFibonacci(int n)
+{
+        return isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4);
+}
+bool odd(int a, int b, int c){
+        if(a+b > c && a+c > b && b+c > a)return true;
+        return false;
+}
+ 
+bool  getS(int n){
+           int s = 0;
+           bool b;
+           while(n){
+               s += n%10;
+               n /= 10;
+           }
+           (s==10)?b = true : b = false;
+           return b;
+ 
+}
+//vector< int > v;
+ 
+bool checkDuplicates( vector<int> array, int n)
+{
+        sort(array.begin(), array.end());
+        int i;
+        for( i = 0; i < n-1; i++)
+        {
+            if( array[i] == array[i+1] )
+            return true;
+ }
+           return false;
+}
+ 
+ll getP(ll d){
+      ll p = 1;
+      while(d){
+        p *= d%10;
+        d /= 10;
+      }
+      return p;
+ 
+}
+ 
+ll prodDig(ll d){
+        ll  p = 1;
+        while(d){
+        if((d%10)){
+        p *= d%10;
+        }
+        d /= 10;
+  }
+       return p;
+}
+ll search(ll n){
+        if(prodDig(n) < 10) return prodDig(n);
+        else return search(prodDig(n));
+}
+string  rev(string s){
+        int n = s.size();
+         string c;
+         for(int i=n-1;i>=0;--i){
+             c+=s[i];
+        }
+         return c;
+}
+ 
+ 
